c37cb9eeec1ecac7f221aa550f2ab6c75a84b780
islam <islamsabir@bk.ru>
2026-01-19 18:17:10 +0300

    Создал форму входа

---
+++ Form.cs
@@ -0,0 +1,12 @@
+class Form
+{
+    static void GetForm()
+    {
+        System.Console.Write("Введите имя: ");
+        string name = Console.ReadLine()!;
+        System.Console.Write("Введите фамилию: ");
+        string surname = Console.ReadLine()!;
+        System.Console.WriteLine($"Ваше имя: {name}\nВаша фамилия: {surname}");
+        Console.ReadLine();
+    }
+}
